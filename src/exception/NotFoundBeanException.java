e3c33acecf7eaa84b93c17117d20beb5a7c4492a
49EHyeon42 <49ehyeon42@gmail.com>
2024-05-21 22:38:41 +0900

    fix: 2차 상속 급하게 추가

--- src/exception/NotFoundBeanException.java
+++ src/exception/NotFoundBeanException.java
@@ -1,6 +1,6 @@
 package exception;
 
-public class NotFoundBeanException extends Exception {
+public class NotFoundBeanException extends NotFoundException {
 
     public NotFoundBeanException() {
         super("<<system>> 해당 원두가 없습니다!");
--- src/exception/NotFoundEmployeeException.java
+++ src/exception/NotFoundEmployeeException.java
@@ -1,6 +1,6 @@
 package exception;
 
-public class NotFoundEmployeeException extends Exception {
+public class NotFoundEmployeeException extends NotFoundException {
 
     public NotFoundEmployeeException() {
         super("해당 직원이 없습니다!");
